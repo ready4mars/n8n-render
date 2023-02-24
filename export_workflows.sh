a75905a224a56880ac30d7820899152640dd9986
Ian Beltrão <74476396+ianbeltrao@users.noreply.github.com>
2023-02-24 11:18:33 +0000

    Update export_workflows.sh

--- export_workflows.sh
+++ export_workflows.sh
@@ -18,5 +18,8 @@ docker run \
     -e DB_POSTGRESDB_SCHEMA \
     -e DB_POSTGRESDB_PASSWORD \
     -v $EXPORT_ROOT:/backup \
+    -e N8N_DATA_FOLDER=/data/db \  # set the data folder path
+    -v /data/db:/root/.n8n \  # mount the data folder as a volume
+    -v $EXPORT_ROOT:/backup \
     -u node \
     n8nio/n8n n8n export:workflow --backup --output=/backup/$EXPORT_DIR/
